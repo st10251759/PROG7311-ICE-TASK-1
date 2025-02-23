c1aa0bd5facd218f2aaaa8e436ae5a07747c97b9
Cameron Chetty <ST10251759@vcconnect.edu.za>
2025-02-23 17:14:46 +0200

    Added a task nodel that will link to the db

---
+++ Models/ToDoTask.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace st10251759_prog7311_ice_task_1.Models
+{
+    public class ToDoTask
+    {
+        //task identifier
+        public int Id { get; set; }
+
+        //required field - title of task
+        [Required]
+        public string Title { get; set; }
+
+        //task description to further explain task
+        public string Description { get; set; }
+
+        //task due date
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        //boolean for whether task is completed or not
+        public bool IsCompleted { get; set; }
+    }
+}
